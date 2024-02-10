c02fa035a679e41e88639191f8ceafbfa897f7c6
thanhsontran <son.tranthanh491@gmail.com>
2024-02-10 15:24:28 +0700

    first commit

---
+++ src/main/java/spring5/guru/sfgdi/SfgDiApplication.java
@@ -0,0 +1,13 @@
+package spring5.guru.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring5/guru/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring5.guru.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
